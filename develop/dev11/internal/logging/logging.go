897ef042718f8e618aa019958385ea2e1f36be98
Idris Akkulov <i.akkulov@gmail.com>
2024-01-25 20:59:58 +0300

    logging

---
+++ develop/dev11/internal/logging/logging.go
@@ -0,0 +1,14 @@
+package logging
+
+import (
+	"log"
+	"net/http"
+)
+
+// LogRequest ...
+func LogRequest(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Received request: %s %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	}
+}
